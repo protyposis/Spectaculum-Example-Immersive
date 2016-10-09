b7e7090ab118e3e16dc343005de5ad3770ad0daa
Mario Guggenberger <mg@protyposis.net>
2016-10-09 22:30:26 +0200

    Setup layout

--- app/src/main/java/net/protyposis/android/spectaculum/sample/immersive/MainActivity.java
+++ app/src/main/java/net/protyposis/android/spectaculum/sample/immersive/MainActivity.java
@@ -3,12 +3,20 @@ package net.protyposis.android.spectaculum.sample.immersive;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.exoplayer2.ui.PlaybackControlView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SpectaculumView mSpectaculumView;
+    private PlaybackControlView mPlaybackControlView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get view references from layout
+        mSpectaculumView = (SpectaculumView) findViewById(R.id.spectaculumview);
+        mPlaybackControlView = (PlaybackControlView) findViewById(R.id.playbackcontrolview);
     }
 }
-
