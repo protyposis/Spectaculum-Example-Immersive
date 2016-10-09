03c8eab46f854369ed85ab34b1877731a2ddb0c0
Mario Guggenberger <mg@protyposis.net>
2016-10-09 22:30:41 +0200

    Configure Spectaculum view

--- app/src/main/java/net/protyposis/android/spectaculum/sample/immersive/MainActivity.java
+++ app/src/main/java/net/protyposis/android/spectaculum/sample/immersive/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 import com.google.android.exoplayer2.ui.PlaybackControlView;
 
+import net.protyposis.android.spectaculum.SpectaculumView;
+import net.protyposis.android.spectaculum.effects.ImmersiveEffect;
+import net.protyposis.android.spectaculum.effects.ImmersiveTouchNavigation;
+
 public class MainActivity extends AppCompatActivity {
 
     private SpectaculumView mSpectaculumView;
@@ -18,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
         // Get view references from layout
         mSpectaculumView = (SpectaculumView) findViewById(R.id.spectaculumview);
         mPlaybackControlView = (PlaybackControlView) findViewById(R.id.playbackcontrolview);
+
+        // Setup Spectaculum view for immersive content
+        ImmersiveEffect immersiveEffect = new ImmersiveEffect(); // create effect instance
+        mSpectaculumView.addEffect(immersiveEffect); // add effect to view
+        mSpectaculumView.selectEffect(0); // activate effect
+
+        // Setup Spectaculum immersive viewport touch navigation
+        ImmersiveTouchNavigation immersiveTouchNavigation = new ImmersiveTouchNavigation(mSpectaculumView);
+        immersiveTouchNavigation.attachTo(immersiveEffect);
+        immersiveTouchNavigation.activate(); // enable touch navigation
     }
 }
